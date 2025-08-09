3af823cc0dad09d141794c4d3c287e6a6cae58cb
vikash s <119433834+vikashsenthil21@users.noreply.github.com>
2025-08-09 18:24:22 +0530

    Add files via upload

---
+++ Keylogger-Mini-Project-main/Keylogger-Mini-Project-main/threat_signatures.py
@@ -0,0 +1,4 @@
+# threat_signatures.py
+KNOWN_KEYLOGGERS = ["keylogger.exe", "kl.exe", "logger.dll", "system-log.exe"]
+KNOWN_MALWARE = ["malware.exe", "trojan.exe", "virus.py", "exploit.dll"]
+SUSPICIOUS_PATHS = ["C:/Windows/System32/drivers/etc", "C:/Users/", "C:/Windows/Temp"]
